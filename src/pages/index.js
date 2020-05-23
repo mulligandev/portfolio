0399f8d66f7068e706c7b68c81883a95fbc78ae0
Mike Mulligan <mkmllgn@gmail.com>
2020-05-23 11:34:53 -0400

    Minor updates for a starting point

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,28 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi there, I'm Mike</h1>
+    <p>
+      I'm a developer located in Upstate New York who's passionate about
+      modernizing the web
+    </p>
+    <ul>
+      <li>
+        Email: <a href="mailto:mkmllgn@gmail.com">mkmllgn@gmail.com</a>
+      </li>
+      <li>
+        Linkedin:{" "}
+        <a href="https://www.linkedin.com/in/michaelmulligan94/">
+          Mike Mulligan
+        </a>
+      </li>
+      <li>
+        Twitter: <a href="https://twitter.com/mulligandev">@mulligandev</a>
+      </li>
+      <li>
+        Github: <a href="https://github.com/mulligandev">@mulligandev</a>
+      </li>
+    </ul>
   </Layout>
 )
 

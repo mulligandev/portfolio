be641dcfebad32556c3b907f3358d63cc2070d77
Mike Mulligan <mkmllgn@gmail.com>
2020-10-31 23:07:59 -0400

    Updated the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,9 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="SOS" />
+    <h1>404: You are lost...</h1>
+    <p>You have wander off the beaten path. Please go back!</p>
   </Layout>
 )
 
